6344d989c5f4a4c45caf4a0f0ffaa457c7b95a99
André Laugks <alaugks@gmail.com>
2026-01-30 13:57:42 +0100

    Update login redirect to include prompt parameter
    - Modify wp_redirect to pass 'prompt' => 'login' in Authentication.php

--- src/Actions/Authentication.php
+++ src/Actions/Authentication.php
@@ -546,7 +546,7 @@ final class Authentication extends Base
             exit;
         }
 
-        wp_redirect($this->getSdk()->login());
+        wp_redirect($this->getSdk()->login(null, ['prompt' => 'login']));
         exit;
     }
 
