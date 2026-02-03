cc05f86537c557bf32b474cbf7f957fa4cc3866e
André Laugks <alaugks@gmail.com>
2026-02-03 14:44:50 +0100

    Force replace cendor package

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,18 +33,6 @@ class ComposerAutoloaderInit748a1c383c71d724e34c2415ab177323
 
         $loader->register(true);
 
-        $filesToLoad = \Composer\Autoload\ComposerStaticInit748a1c383c71d724e34c2415ab177323::$files;
-        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-                require $file;
-            }
-        }, null, null);
-        foreach ($filesToLoad as $fileIdentifier => $file) {
-            $requireFile($fileIdentifier, $file);
-        }
-
         return $loader;
     }
 }
